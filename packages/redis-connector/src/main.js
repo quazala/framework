6a4694a4e87a0d99bd7afc458e979fe1ac32da12
lemarc <lemarc.dev@gmail.com>
2024-09-03 17:00:51 +0200

    remove comments

--- packages/redis-connector/src/main.js
+++ packages/redis-connector/src/main.js
@@ -7,17 +7,15 @@ export const connect = async (ctx) => {
   try {
     const client = createClient({
       url: ctx.env.redis.url,
-      // Add other configuration options as needed
     });
 
-    // Set up event listeners
     client.on('error', (err) => logger.error(`Redis Client Error: ${err}`, 'error'));
     client.on('ready', () => logger.info('Redis Client Ready'));
     client.on('connect', () => logger.info('Redis Client Connected'));
     client.on('reconnecting', () => logger.info('Redis Client Reconnecting'));
     client.on('end', () => logger.info('Redis Client Connection Ended'));
 
-    // Connect to the Redis server
+
     await client.connect();
 
     logger.info('Successfully connected to Redis');
